cfa70701684d82de7a09af2c007e7724ea7e2ea3
guyskv <guyskv@gmail.com>
2023-03-08 18:40:50 +0200

    Initial commit

---
+++ app/src/main/java/guy/capsulab/class23b_and_2/MainActivity.java
@@ -0,0 +1,14 @@
+package guy.capsulab.class23b_and_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
