dc97b5fde5fcc40a06dc78d3f5a13e52844b3599
guyskv <guyskv@gmail.com>
2023-03-08 18:59:24 +0200

    2

--- app/src/main/java/guy/capsulab/class23b_and_2/MainActivity.java
+++ app/src/main/java/guy/capsulab/class23b_and_2/MainActivity.java
@@ -3,12 +3,39 @@ package guy.capsulab.class23b_and_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText main_EDT_name;
+    private EditText main_EDT_gross;
+    private MaterialButton main_BTN_calculate;
+    private MaterialTextView main_LBL_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_EDT_name = findViewById(R.id.main_EDT_name);
+        main_EDT_gross = findViewById(R.id.main_EDT_gross);
+        main_BTN_calculate = findViewById(R.id.main_BTN_calculate);
+        main_LBL_result = findViewById(R.id.main_LBL_result);
+
+        main_BTN_calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
     }
+
+    private void calculate() {
+
+    }
+
 }
